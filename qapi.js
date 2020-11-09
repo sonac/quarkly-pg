94eb4ef10c733ae367d4d9078c24b746a276db66
Andrii <sonag007@gmail.com>
2020-11-09 11:38:19 +0100

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa91bd08996cc001edc9b8d",
+				"5fa91bd08996cc001edc9b8f"
+			]
+		},
+		"5fa91bd08996cc001edc9b8d": {
+			"id": "5fa91bd08996cc001edc9b8d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa91bd08996cc001edc9b8f": {
+			"id": "5fa91bd08996cc001edc9b8f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
